fc9617064f5e687de043dae0e896d75afa4a05ee
Dizolivemint <Dizolivemint@koji-projects.com>
2019-05-27 03:45:24 +0000

    Initial version of Digital Business Card

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# Koji React Scaffold
+# Digital Business Card
 
-Welcome to your new Koji React Project.
+Welcome to your digital business card!
+* Easily customize it to your specifications.
+* Add your own domain.
+* PWA supported (i.e. Add to homescreen, push notifications, offline usage)
+
+For easy access, add digital business cards to a busniess cards folder on your homescreen.
+* [Pixel directions](https://www.youtube.com/watch?v=LEiGDtbYN9s)
+
+Change out icons using the included [react-icons library](https://react-icons.netlify.com/#/icons/fa)
 
 -------
 # Where to go
@@ -8,8 +16,9 @@ Welcome to your new Koji React Project.
 2. Your Backend code is located at /backend/routes and there is an example route at [SampleRoute](#~/backend/routes/SampleRoute/index.js)
 3. 3 Visual Customization Controls (VCCs) have been added to the template. These can be quickly modified to your applicaiton
     - [Colors](#~/.koji/customization/colors.json!visual) - Change the text color, background color, & link text color.
-    - [Images](#~/.koji/customization/images.json!visual) - Change the spinning icon.
-    - [Strings](#~/.koji/customization/strings.json!visual) - Change the page content text & link text.
+    - [Images](#~/.koji/customization/images.json!visual) - Change out the logo and background. 
+    - [Strings](#~/.koji/customization/strings.json!visual) - Change the out the company information.
+4. Find logos, backgrounds, and any other assets you may need at [Awesome Design Tools](https://flawlessapp.io/designtools)
 
 ## Any Questions?
 
--- frontend/pages/HomePage/index.js
+++ frontend/pages/HomePage/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import Koji from 'koji-tools';
+import MaterialIcon, {colorPalette} from 'material-icons-react';
+import { FaSearchLocation, FaPhone } from 'react-icons/fa';
+import { IoIosMail } from "react-icons/io";
+
 
 const Container = styled.div`
     background-color: ${() => Koji.config.colors.backgroundColor};
+    ${() => Koji.config.layout.useGradient && `
+        background: linear-gradient(${Koji.config.layout.angleGradient}deg, ${Koji.config.colors.gradientColor01} 0%, ${Koji.config.colors.gradientColor02} 35%, ${Koji.config.colors.gradientColor03} 100%)`};
+    
+    ${() => Koji.config.layout.useBackgroundImage && `
+        background-image: url(${Koji.config.images.background})`};
+        
     min-height: 100vh;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    flex-wrap: wrap;
     align-items: center;
-    justify-content: center;
     font-size: calc(10px + 2vmin);
     color: ${() => Koji.config.colors.textColor};
     text-align: center;
+    justify-content: center;
+    @media (max-width: 1280px) {
+        flex-direction: column;
+    }
+`;
+
+const H1Name = styled.h1`
+    font-size: calc(10px + 4vmin);
+    letter-spacing: ${() => Koji.config.layout.nameLetterSpacing};
+    text-indent: ${() => Koji.config.layout.nameLetterSpacing};
+    width: 100%;
+    margin: auto;
+    text-align: center;
+`;
+
+const H2Title = styled.p`
+    font-size: calc(10px + 1vmin);
+    letter-spacing: ${() => Koji.config.layout.titleLetterSpacing};
+    text-indent: ${() => Koji.config.layout.titleLetterSpacing};
+    margin-top: 0
 `;
 
 const AppLogoSpin = keyframes`
@@ -28,18 +58,78 @@ const Content = styled.div`
 `;
 
 const Link = styled.a`
-  color: ${() => Koji.config.colors.linkColor};
+    color: ${() => Koji.config.colors.linkColor};
+    text-decoration: none;
+    padding: 0 8px 8px 8px;
+    display: flex;
+    text-align: ${() => Koji.config.layout.alignAddress};
+    flex-wrap: wrap;
+    width: ${props => props.colWidth};
+    margin: 0 0 1em 0;
 `;
 
-const Icon = styled.div`
-    animation: ${AppLogoSpin} infinite 20s linear;
-    height: 40vmin;
-    width: 60vmin;
+const Logo = styled.img`
+    height: 25vmin;
     pointer-events: none;
-    background-image: url(${() => Koji.config.images.icon});
-    background-size: cover;
-    margin-bottom: 16px;
 `;
+
+const Column = styled.div`
+    width: ${props => props.colWidth};
+    align-self: center;
+    display: flex;
+    justify-content: center;
+`;
+
+const Icon = styled(Column)`
+
+`;
+
+const Title = styled(Column)`
+    
+`;
+
+const Company = styled(Column)`
+    padding: 0 8px 8px 8px;
+    display: flex;
+    text-align: ${() => Koji.config.layout.alignAddress};
+    flex-wrap: wrap;
+    @media (min-width: 1280px) {
+        width: 50%
+    }
+`;
+
+const Address = styled(Link)``;
+
+const Phone = styled(Link)``;
+
+const Email = styled(Link)``;
+
+function memoize(func) {
+
+  let cache = new Map();
+
+  const memoized = function (...args) {
+
+    let key = args[0];
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    let result = func.apply(this, args);
+    cache.set(key, result);
+    return result;
+  };
+
+  return memoized;
+}
+
+function getMapUri() {
+  let mapUri = `https://www.google.com/maps/place/${Koji.config.strings.companyAddress},${Koji.config.strings.companyCity},${Koji.config.strings.companyState}+${Koji.config.strings.companyZip}`
+  return mapUri.replace(/ /g, "+");
+}
+
+let memoizedUri = memoize(getMapUri);
 
 class HomePage extends React.Component {
     constructor(props) {
@@ -47,6 +137,8 @@ class HomePage extends React.Component {
 
         this.state = {
             response: '',
+            icons: Koji.config.layout.icons,
+            mapUri: "https://www.google.com/maps"
         };
     }
 
@@ -59,21 +151,94 @@ class HomePage extends React.Component {
         Koji.on('change', () => {
             this.forceUpdate();
         })
+
+        this.setState({mapUri: getMapUri()});
     }
 
     render() {
         return (
             <Container>
-                <Icon />
-                <Content>{this.state.response}</Content>
-                <Content>{Koji.config.strings.content}</Content>
                 <Link
-                href="https://reactjs.org"
+                href={Koji.config.strings.websiteUrl}
                 target="_blank"
                 rel="noopener noreferrer"
                 >
-                    {Koji.config.strings.linkText}
-                </Link>
+
+                    <Logo src={Koji.config.images.logo} />
+                    
+                </Link>              
+                <Company>
+                    <Title colWidth="100%">
+
+                        <H1Name>{Koji.config.strings.name}</H1Name>
+
+                    </Title>
+                    <Title colWidth="100%">
+
+                        <H2Title>{Koji.config.strings.title}</H2Title>  
+
+                    </Title>
+                    <Address 
+                    href={this.state.mapUri}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >    
+                        <Icon colWidth="2em">
+
+                            {(this.state.icons) &&
+                                <FaSearchLocation />
+                            }
+
+                        </Icon>
+                        <Column colWidth="auto">
+
+                                {Koji.config.strings.companyAddress}
+                                <br/>
+                                {Koji.config.strings.companyCity},&nbsp;
+                                {Koji.config.strings.companyState}&nbsp;
+                                {Koji.config.strings.companyZip}
+
+
+                        </Column>
+                    </Address>
+
+                    <Phone 
+                    href={"tel:" + Koji.config.strings.companyPhone}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                        <Icon colWidth="2em">
+
+                            {(this.state.icons) &&
+                                <FaPhone />
+                            }
+
+                        </Icon>
+                        <Column colWidth="auto">
+
+                                {Koji.config.strings.companyPhone}
+
+                        </Column>
+                    </Phone>
+                    <Email 
+                    href={"mailto:" + Koji.config.strings.email}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                        <Icon colWidth="2em">
+
+                            {(this.state.icons) &&
+                                <IoIosMail />
+                            }
+
+                        </Icon>
+                        <Column colWidth="auto">
+
+                                {Koji.config.strings.email}
+
+                        </Column>
+                    </Email>
+                </Company>
             </Container>
         );
     }
