9305333755b5a9975dcc609e126bf54de8cbe4f8
Dizolivemint <Dizolivemint@koji-projects.com>
2019-06-03 01:44:00 +0000

    Koji deploy

--- frontend/.internals/webpack.production.js
+++ frontend/.internals/webpack.production.js
@@ -44,7 +44,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react', { modules: false }]
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       },
